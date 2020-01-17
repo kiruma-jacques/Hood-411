d5efc0e88fe5929f87c32630bd6e6be7777f84ee
jakesIII <jayruma@yahoo.com>
2020-01-17 21:27:50 +0300

    create profile model

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from pyuploadcare.dj.models import ImageField
+
 # Create your models here.
+class Profile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    avi=ImageField()
+    bio=models.CharField(max_length=240)
+    
 class Hood(models.Model):
     user=models.OneToOneField(User, on_delete=models.CASCADE, related_name='Hood')
     name=models.CharField(max_length=200)
--- hoodapp/views.py
+++ hoodapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Hood,Biz,Member
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
